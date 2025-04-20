626257e3939f66261bb7418a81879d4bb75a7799
Aizada Berdibekova <aizada.berdibekova_2025@ucentralasia.org>
2025-04-20 17:09:27 +0600

    deployment

--- agroconnect_project/settings.py
+++ agroconnect_project/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -156,3 +156,16 @@ REST_FRAMEWORK = {
 
 # Enable cookies in cross-site requests
 CORS_ALLOW_CREDENTIALS = True
+
+
+# if not DEBUG:
+#     CSRF_TRUSTED_ORIGINS = ['', '']
+#     # SECURE_SSL_REDIRECT = True
+#     SESSION_COOKIE_SECURE = True
+#     CSRF_COOKIE_SECURE = True
+#     SECURE_BROWSER_XSS_FILTER = True
+#     SECURE_CONTENT_TYPE_NOSNIFF = True
+# #     SECURE_HSTS_SECONDS = 31536000  # 1 year
+# #     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# #     SECURE_HSTS_PRELOAD = True
+#     X_FRAME_OPTIONS = 'DENY'
